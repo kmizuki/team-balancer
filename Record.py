eabf812089cfb4343913c9ec626f936a211c0ac1
“k_mizuki” <k_mizuki@koto.co.jp>
2022-07-13 23:50:41 +0900

    :bug: fix read file ttl

--- Record.py
+++ Record.py
@@ -41,6 +41,7 @@ credentials = service_account.Credentials.from_service_account_info(st.secrets["
 client = storage.Client(credentials=credentials)
 
 
+@st.experimental_memo(ttl=600)
 def read_file(bucket_name, file_path):
     bucket = client.bucket(bucket_name)
     content = bucket.blob(file_path).download_as_bytes()
